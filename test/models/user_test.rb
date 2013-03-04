d6beb26bdce2fd2c1d2108fe3c11f35b54d87ded
Dominik Bamberger <dmayer@suse.de>
2013-03-04 14:51:37 +0100

    test adding groups to a user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,8 +1,17 @@
 require 'minitest_helper'
 
 describe 'User' do
+  subject { FactoryGirl.create :user }
+
   it 'uses factory_girl' do
-    user = FactoryGirl.create :user
-    user.login.wont_be_nil
+    subject.login.wont_be_nil
+  end
+
+  it 'allows to add groups' do
+    group = FactoryGirl.create :group
+    subject.groups.must_be_empty
+    subject.groups << group
+    subject.groups.wont_be_empty
+    subject.groups.first.must_equal group
   end
 end
