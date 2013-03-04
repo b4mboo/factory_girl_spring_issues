69aa0ae13b0307cb3121639b775e1f1f03f6d6ff
Dominik Bamberger <dmayer@suse.de>
2013-03-04 11:24:03 +0100

    initialize rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FactoryGirlSpringIssues::Application.config.secret_token = '1a727ace449588132df191823e3c8ae57daa58211220d468ec6be65373c39c6a671aa1d8ab8a457ef843bcc4cd2463ccd4c40aa4d7e10dec985ce20cf9786a9e'
